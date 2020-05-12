08e68981fcfc2c2ea9bcd8e863c00230b3ede523
luward <luis.wardenbach@uni-bonn.de>
2020-05-13 00:34:35 +0200

    fixed uncerainty_quantification functions

--- tespy/uncerainty_quantification.py
+++ tespy/uncerainty_quantification.py
@@ -10,14 +10,14 @@ The Borehole function models water flow through a borehole. Its simplicity and q
 def borehole(x):
     r_w = np.random.normal(0.1, 0.0161812)
     r = np.random.lognormal(7.71, 1.0056)
-    T_u = np.uniform(63070, 115600)
-    H_u = np.uniform(990, 1110)
-    T_l = np.uniform(63.1, 116)
-    H_l = np.uniform(700, 820)
-    L = np.uniform(1120, 1680)
-    K_w = np.uniform(9855, 12045)
+    T_u = np.random.uniform(63070, 115600)
+    H_u = np.random.uniform(990, 1110)
+    T_l = np.random.uniform(63.1, 116)
+    H_l = np.random.uniform(700, 820)
+    L = np.random.uniform(1120, 1680)
+    K_w = np.random.uniform(9855, 12045)
 
-    a = 2 * math.pi * T_U * (H_u - H_l)
+    a = 2 * math.pi * T_u * (H_u - H_l)
     b = np.log(r / r_w)
     c = (2 * L * T_u) / (b * r_w ** 2 * K_w)
     d = T_u / T_l
@@ -31,6 +31,6 @@ The Ishigami function of Ishigami & Homma (1990) is used as an example for uncer
 """
 
 
-def ishigami(x_1, y, z, a=7, b=0.1):
+def ishigami(x, y, z, a=7, b=0.1):
     rslt = (1 + b * z ** 4) * np.sin(x) + a * np.sin(y) ** 2
     return rslt
