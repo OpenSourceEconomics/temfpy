32f354f9b5ea9f69914d2a3bae52fb256aa09c6c
luward <luis.wardenbach@uni-bonn.de>
2020-05-13 00:26:12 +0200

    restructuring, added uncertainty_quantification.py

--- ackley.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import math
-
-def ackley(x, a = 20, b = 0.2, c = 2*math.pi):
-    n = len(x)
-    function = a + math.exp(1) - (a * (math.exp(-b * math.sqrt(1/n * np.sum(np.square(x)))))) - math.exp(1/n * np.sum(np.cos(c*x)))
-    return function
