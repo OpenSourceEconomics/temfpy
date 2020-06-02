e25b76f5ea15c44e957c5c0fbdf0abece2e360c5
milakis <liudmila.kiseleva.rus@gmail.com>
2020-06-02 23:43:44 +0200

    corrected black

--- temfpy/optimization.py
+++ temfpy/optimization.py
@@ -58,8 +58,10 @@ def ackley(x, a=20, b=0.2, c=2 * math.pi):
     """
     n = len(x)
 
-    rslt = a + math.exp(1) - (a * (math.exp(-b * math.sqrt(1 / n * np.sum(np.square(x))))))
-    rslt -= math.exp(1 / n * np.sum(np.cos(np.multiply(c,x))))
+    rslt = (
+        a + math.exp(1) - (a * (math.exp(-b * math.sqrt(1 / n * np.sum(np.square(x))))))
+    )
+    rslt -= math.exp(1 / n * np.sum(np.cos(np.multiply(c, x))))
 
     return rslt
 
