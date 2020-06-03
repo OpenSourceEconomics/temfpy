3425d4d4b9e911b8b619b391c7f68fe9a3483e9b
loikein <wanleiqiong@gmail.com>
2020-06-03 17:49:57 +0200

    fix pytest

--- temfpy/tests/test_optimization.py
+++ temfpy/tests/test_optimization.py
@@ -1,35 +1,31 @@
-"""Tests for optimization module.
-"""
+"""Tests for optimization module."""
 import numpy as np
 
 from hypothesis.extra.numpy import arrays
 from hypothesis.strategies import floats
+from hypothesis.strategies import integers
 from hypothesis import given
 
 from temfpy.optimization import ackley
 from temfpy.optimization import rastrigin
 
 
-def get_strategies(name, x):
-    if name == "ackley":
+def get_strategies(name):
+    if (name == "ackley") or (name == "rastrigin"):
         valid_floats = floats(-10000, 10000, allow_nan=False, allow_infinity=False)
-        x_strategy = arrays(np.float, len(x), elements=valid_floats)
-        strategy = (x_strategy, valid_floats)
-    elif name == "rastrigin":
-        valid_floats = floats(-10000, 10000, allow_nan=False, allow_infinity=False)
-        x_strategy = arrays(np.float, len(x), elements=valid_floats)
-        strategy = (x_strategy, valid_floats)
+        x_strategy = arrays(np.float, shape=integers(1, 10), elements=valid_floats)
+        strategy = x_strategy
     else:
         raise NotImplementedError
 
     return strategy
 
 
-@given(*get_strategies("ackley", x))
-def test_ackley(x, a, b, c):
-    ackley(x, a, b, c)
+@given(get_strategies("ackley"))
+def test_ackley(x):
+    ackley(x)
 
 
-@given(*get_strategies("rastrigin", x))
-def test_rastrigin(x, a):
-    rastrigin(x, a)
+@given(get_strategies("rastrigin"))
+def test_rastrigin(x):
+    rastrigin(x)
