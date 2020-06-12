8b660972eef7ad4693ff455c8c02029e98de956b
Mila Kiseleva <49441004+milakis@users.noreply.github.com>
2020-06-13 01:28:38 +0200

    Update test_optimization.py

--- temfpy/tests/test_optimization.py
+++ temfpy/tests/test_optimization.py
@@ -11,7 +11,11 @@ from temfpy.optimization import rastrigin
 
 
 def get_strategies(name):
-    if (name == "ackley") or (name == "rastrigin"):
+    if name == "ackley": 
+        valid_floats = floats(-10000, 10000, allow_nan=False, allow_infinity=False)
+        x_strategy = arrays(np.float, shape=integers(1, 10), elements=valid_floats)
+        strategy = (x_strategy, valid_floats, valid_floats, valid_floats)
+    elif name == "rastrigin":
         valid_floats = floats(-10000, 10000, allow_nan=False, allow_infinity=False)
         x_strategy = arrays(np.float, shape=integers(1, 10), elements=valid_floats)
         strategy = (x_strategy, valid_floats)
@@ -22,8 +26,8 @@ def get_strategies(name):
 
 
 @given(get_strategies("ackley"))
-def test_ackley(x, a):
-    ackley(x, a)
+def test_ackley(x, a, b, c):
+    ackley(x, a, b, c)
 
 
 @given(get_strategies("rastrigin"))
