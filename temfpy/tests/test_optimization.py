925598dee2e1a42f83773f91210a280405184976
milakis <liudmila.kiseleva.rus@gmail.com>
2020-06-13 01:39:35 +0200

    black

--- temfpy/tests/test_optimization.py
+++ temfpy/tests/test_optimization.py
@@ -11,7 +11,7 @@ from temfpy.optimization import rastrigin
 
 
 def get_strategies(name):
-    if name == "ackley": 
+    if name == "ackley":
         valid_floats = floats(-10000, 10000, allow_nan=False, allow_infinity=False)
         x_strategy = arrays(np.float, shape=integers(1, 10), elements=valid_floats)
         strategy = (x_strategy, valid_floats, valid_floats, valid_floats)
