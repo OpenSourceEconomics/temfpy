0a6e69a67484b85f659c4d9844d5dba56cd73348
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-10-19 20:51:53 +0200

    Add files via upload

--- temfpy/tests/test_econometrics.py
+++ temfpy/tests/test_econometrics.py
@@ -5,6 +5,7 @@ import pandas as pd
 import patsy
 from estimagic.optimization.optimize import maximize
 from hypothesis import given
+from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
 from hypothesis.strategies import floats
 from hypothesis.strategies import integers
@@ -16,7 +17,7 @@ from temfpy.econometrics import multinomial_probit_loglike
 from temfpy.econometrics import multinomial_probit
 
 
-def data_generation(n_obs, n_var, choices, beta_low = -3, beta_high = 3):
+def data_generation(n_obs, n_var, choices, beta_low=-3, beta_high=3):
     r"""Multinomial probit model. Simple Random DGP.
 
     .. math::
@@ -46,30 +47,60 @@ def data_generation(n_obs, n_var, choices, beta_low = -3, beta_high = 3):
     df: pandas.DataFrame
     Data frame containing the endogenous variable Y and the exogenous variables.
     """
-    X = np.random.normal(size = n_obs*n_var).reshape((n_obs, n_var))
-    betas = np.random.uniform(low = beta_low, high = beta_high, size = choices*n_var).reshape(n_var, choices)
-    Y_latent = np.dot(X, betas) + np.random.normal(size = n_obs*choices).reshape(n_obs, choices)
-    Y = np.argmax(Y_latent, axis = 1) + 1
-    columns = [f'v{num}' for num in range(n_var)]
-    df = pd.DataFrame(X, columns = columns)
-    df['Y'] = Y
-    
+    X = np.random.normal(size=n_obs * n_var).reshape((n_obs, n_var))
+    betas = np.random.uniform(
+        low=beta_low, high=beta_high, size=choices * n_var
+    ).reshape(n_var, choices)
+    Y_latent = np.dot(X, betas) + np.random.normal(size=n_obs * choices).reshape(
+        n_obs, choices
+    )
+    Y = np.argmax(Y_latent, axis=1) + 1
+    columns = [f"v{num}" for num in range(n_var)]
+    df = pd.DataFrame(X, columns=columns)
+    df["Y"] = Y
+
     return df
 
-cov_strategy = arrays(np.str, 1, elements=["iid", "free"])
-integration_strategy = arrays(np.str, 1, elements=["mc_integration", "smooth_mc_integration", "gauss_integration"])
-algorithm_strategy = arrays(np.str, 1, elements=["scipy_L-BFGS-B", "scipy_SLSQP", "nlopt_bobyqa", "nlopt_newuoa_bound"])
-n_obs_strategy = arrays(np.int, 1, elements=integers(50,500))
-n_var_strategy = arrays(np.int, 1, elements=integers(2,10))
-choices = arrays(np.int, 1, elements=integers(2,7))
 
-strategy = (n_obs_strategy, n_var_strategy , choices, cov_strategy, integration_strategy, algorithm_strategy)
+cov_strategy = arrays(np.str, 1, elements=st.sampled_from(["iid", "free"]))
+integration_strategy = arrays(
+    np.str,
+    1,
+    elements=st.sampled_from(
+        ["mc_integration", "smooth_mc_integration", "gauss_integration"]
+    ),
+)
+algorithm_strategy = arrays(
+    np.str,
+    1,
+    elements=st.sampled_from(
+        ["scipy_L-BFGS-B", "scipy_SLSQP", "nlopt_bobyqa", "nlopt_newuoa_bound"]
+    ),
+)
+n_obs_strategy = arrays(np.int, 1, elements=integers(50, 500))
+n_var_strategy = arrays(np.int, 1, elements=integers(2, 10))
+choices = arrays(np.int, 1, elements=integers(2, 7))
+
+strategy = (
+    n_obs_strategy,
+    n_var_strategy,
+    choices,
+    cov_strategy,
+    integration_strategy,
+    algorithm_strategy,
+)
+
 
 @given(*strategy)
-def test_multinomial_probit(n_obs_strategy, n_var_strategy , choices, cov_structure, integration_method, algorithm): 
-    data = data_generation(n_obs_strategy, n_var_strategy , choices)
+def test_multinomial_probit(
+    n_obs_strategy,
+    n_var_strategy,
+    choices,
+    cov_structure,
+    integration_method,
+    algorithm,
+):
+    data = data_generation(n_obs_strategy, n_var_strategy, choices)
     all_columns = "+".join(data.columns.difference(["Y"]))
     formula = "Y~" + all_columns
     multinomial_probit(formula, data, cov_structure, integration_method, algorithm)
-    
-    
