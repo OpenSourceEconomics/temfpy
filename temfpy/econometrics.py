777f2c1d8d20cdbefa731df9ccf158d9a1c67a28
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-10-20 15:17:36 +0200

    Add files via upload

--- temfpy/econometrics.py
+++ temfpy/econometrics.py
@@ -218,8 +218,8 @@ def multinomial_probit(formula, data, cov_structure, integration_method, algorit
     r"""Multinomial probit model.
 
     .. math::
-        \text{for} i = 1, \dots, n, j = 1, \dots, m \text{ and }
-        \beta_j, X_i \in \mathbb{R}^{k}
+        \text{for } i = 1, \dots, n, j = 1, \dots, m \text{ and }
+        \beta_j, X_i \in \mathbb{R}^{k} \\
         Y_i^{*j} &= X_i^T \beta_j + \varepsilon_j \\
         Y_i &= \underset{}{\mathrm{argmax}} \{Y_i^{*j} \mid j = 1, \dots, m \}
 
