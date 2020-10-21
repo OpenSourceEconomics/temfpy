c81c92eb602b3bf8556e8ba01744e6a5605844c0
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-10-21 16:17:32 +0200

    Add section on nonlinear equations (#31)

---
+++ docs/_static/codes/fig-rosenbrock.py
@@ -0,0 +1,36 @@
+"""Figure of the Ackley function in 3D.
+x1 is evaluated on [-32.768, 32.768]
+x2 is evaluated on [-32.768, 32.768]
+y is the result of applying the Ackley function on each combination of x1 and x2
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+from temfpy.optimization import rosenbrock
+
+x1 = np.linspace(-3, 3, 500)
+x2 = np.linspace(-3, 3, 500)
+
+xvalues = []
+array = []
+for i in range(0, len(x1)):
+    for j in range(0, len(x2)):
+        array = [x1[i], x2[j]]
+        xvalues.append(array)
+
+yvalues = np.linspace(0, 0, len(xvalues))
+for i, j in zip(xvalues, range(0, len(xvalues))):
+    yvalues[j] = rosenbrock(i)
+
+xvalues = np.array(xvalues)
+xvalues1 = xvalues[:, 0]
+xvalues2 = xvalues[:, 1]
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(xvalues1, xvalues2, yvalues, c=yvalues, cmap="viridis", linewidth=0.05)
+ax.set_xlabel("$x_1$")
+ax.set_ylabel("$x_2$")
+ax.set_zlabel("$f(x_1, x_2)$")
+fig.savefig("fig-rosenbrock")
---
+++ temfpy/tests/test_nonlinear_equations.py
@@ -0,0 +1,76 @@
+"""Tests for nonlinear equations module."""
+import numpy as np
+from hypothesis import given
+from hypothesis.extra.numpy import arrays
+from hypothesis.strategies import floats
+from hypothesis.strategies import integers
+
+from temfpy.nonlinear_equations import broyden
+from temfpy.nonlinear_equations import chandrasekhar
+from temfpy.nonlinear_equations import exponential
+from temfpy.nonlinear_equations import rosenbrock_ext
+from temfpy.nonlinear_equations import trig_exp
+from temfpy.nonlinear_equations import troesch
+
+
+def get_strategies(name):
+    valid_floats = floats(0.01, 10000, allow_nan=False, allow_infinity=False)
+    valid_floats_exp = floats(-100, 500, allow_nan=False, allow_infinity=False)
+    if name == "exponential":
+        x_strategy = arrays(np.float, 3, elements=valid_floats_exp)
+        strategy = (x_strategy, integers(1, 100), integers(1, 100))
+    elif name == "trig_exp":
+        x_strategy = arrays(np.float, 3, elements=valid_floats_exp)
+        a_strategy = arrays(np.float, 9, elements=integers(1, 20))
+        strategy = (x_strategy, a_strategy)
+    elif name == "broyden":
+        x_strategy = arrays(np.float, 3, elements=valid_floats)
+        a_strategy = arrays(np.float, 4, elements=integers(1, 20))
+        strategy = (x_strategy, a_strategy)
+    elif name == "rosenbrock_ext":
+        x_strategy = arrays(np.float, 3, elements=valid_floats)
+        a_strategy = arrays(np.float, 2, elements=integers(1, 20))
+        strategy = (x_strategy, a_strategy)
+    elif name == "troesch":
+        x_strategy = arrays(
+            np.float, 3, elements=floats(-3, 3, allow_nan=False, allow_infinity=False),
+        )
+        strategy = (x_strategy, integers(1, 100), integers(1, 100))
+    elif name == "chandrasekhar":
+        x_strategy = arrays(np.float, 3, elements=valid_floats)
+        y_strategy = arrays(np.float, 3, elements=valid_floats)
+        strategy = (x_strategy, y_strategy, integers(1, 100))
+    else:
+        raise NotImplementedError
+
+    return strategy
+
+
+@given(*get_strategies("exponential"))
+def test_exponential(x, a, b):
+    exponential(x, a, b)
+
+
+@given(*get_strategies("trig_exp"))
+def test_trig_exp(x, a):
+    trig_exp(x, a)
+
+
+@given(*get_strategies("broyden"))
+def test_broyden(x, a):
+    broyden(x, a)
+
+
+@given(*get_strategies("rosenbrock_ext"))
+def test_rosenbrock_ext(x, a):
+    rosenbrock_ext(x, a)
+
+
+@given(*get_strategies("troesch"))
+def test_troesch(x, rho, a):
+    troesch(x, rho, a)
+
+
+@given(*get_strategies("chandrasekhar"))
+def test_chandrasekhar(x, y, c):
+    chandrasekhar(x, y, c)
