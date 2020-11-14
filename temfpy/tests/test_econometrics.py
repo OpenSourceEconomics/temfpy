9356898233d3dcaff134b0533afe246ba0aeb2d7
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-11-14 22:44:30 +0100

    Change variable letters to small letters.

--- temfpy/tests/test_econometrics.py
+++ temfpy/tests/test_econometrics.py
@@ -1,7 +1,8 @@
 """Tests for econometrics module."""
 import numpy as np
 import pandas as pd
-import temfpy.econometrics as tpe
+
+from temfpy.econometrics import multinomial_probit
 
 
 def data_generation(n_obs, n_var, choices, seed, beta_low=-3, beta_high=3):
@@ -20,12 +21,19 @@ def data_generation(n_obs, n_var, choices, seed, beta_low=-3, beta_high=3):
     ----------
     n_obs : int
             number of observations
+
     n_var : int
             number of exogenous variables
+
+    seed : int
+           Number passed to numpy.seed to ensure reproducability.
+
     choices : int
               number of possible discrete outcomes
+
     beta_low : float, optional
                The default is -3
+
     beta_high : float, optional
                 The default is 3
 
@@ -39,13 +47,13 @@ def data_generation(n_obs, n_var, choices, seed, beta_low=-3, beta_high=3):
     betas = np.random.uniform(
         low=beta_low, high=beta_high, size=choices * n_var
     ).reshape(n_var, choices)
-    Y_latent = np.dot(X, betas) + np.random.normal(size=n_obs * choices).reshape(
+    y_latent = np.dot(X, betas) + np.random.normal(size=n_obs * choices).reshape(
         n_obs, choices
     )
-    Y = np.argmax(Y_latent, axis=1) + 1
+    y = np.argmax(y_latent, axis=1) + 1
     columns = [f"v{num}" for num in range(n_var)]
     df = pd.DataFrame(X, columns=columns)
-    df["Y"] = Y
+    df["y"] = y
 
     return df
 
@@ -57,22 +65,22 @@ def fixed_tests(cov_strategy=None, integration_strategy=None, algorithm_strategy
     choices = np.random.randint(2, 6)
 
     if cov_strategy is None:
-        cov_strategy = ["iid", "free"][np.random.randint(0, 2)]
+        cov_strategy = np.random.choice(["iid", "free"])
 
     if integration_strategy is None:
-        integration_strategy = [
+        integration_strategy = np.random.choice([
             "mc_integration",
             "smc_integration",
             "gauss_integration",
-        ][np.random.randint(0, 3)]
+        ])
 
     if algorithm_strategy is None:
-        algorithm_strategy = ["scipy_lbfgsb", "scipy_slsqp"][np.random.randint(0, 2)]
+        algorithm_strategy = np.random.choice(["scipy_lbfgsb", "scipy_slsqp"])
 
     data = data_generation(n_obs_strategy, n_var_strategy, choices, seed=10)
-    all_columns = "+".join(data.columns.difference(["Y"]))
-    formula = "Y~" + all_columns
-    tpe.multinomial_probit(
+    all_columns = "+".join(data.columns.difference(["y"]))
+    formula = "y~" + all_columns
+    multinomial_probit(
         formula, data, cov_strategy, integration_strategy, algorithm_strategy
     )
 
