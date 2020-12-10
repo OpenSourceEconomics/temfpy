0bf6dd244bffa674549223f1eb20fad43ef1e1fe
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-12-10 10:54:40 +0100

    Add integration section (#41)
    Add integration section.

---
+++ docs/_static/codes/fig-continuous_integrand.py
@@ -0,0 +1,39 @@
+"""Figure of the Ackley function in 3D.
+
+x1 is evaluated on [0, 1]
+x2 is evaluated on [0, 1]
+y is the result of applying the Continuous Integrand function on
+each combination of x1 and x2 and u1,u2 = 0.5
+
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+from temfpy.integration import continuous
+
+x1 = np.linspace(0, 1)
+x2 = np.linspace(0, 1)
+
+xvalues = []
+array = []
+for i in range(0, len(x1)):
+    for j in range(0, len(x2)):
+        array = [x1[i], x2[j]]
+        xvalues.append(array)
+
+yvalues = np.linspace(0, 0, len(xvalues))
+for i, j in zip(xvalues, range(0, len(xvalues))):
+    yvalues[j] = continuous(i, [0.5, 0.5], [1, 1])
+
+xvalues = np.array(xvalues)
+xvalues1 = xvalues[:, 0]
+xvalues2 = xvalues[:, 1]
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(xvalues1, xvalues2, yvalues, c=yvalues, cmap="viridis", linewidth=0.05)
+ax.set_xlabel("$x_1$")
+ax.set_ylabel("$x_2$")
+ax.set_zlabel("$f(x_1, x_2)$")
+fig.savefig("fig-continuous_integrand")
