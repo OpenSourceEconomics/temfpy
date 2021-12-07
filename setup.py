482f438041f81af8afab2377909a1e48f3948af1
s6emschw <64075224+s6emschw@users.noreply.github.com>
2021-12-07 19:24:33 +0100

    Bumped temfpy version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ p = find_packages()
 
 setup(
     name="temfpy",
-    version="1.2.0",
+    version="1.2.1",
     description=("Python package with test functions for various numerical components"),
     license="MIT",
     url="https://github.com/OpenSourceEconomics/temfpy",
